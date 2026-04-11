c52b12a1d7722c2d31510f9f57453c773f7d60b0
Manish Sharma <sharmamanishsharma.abc49@gmail.com>
2026-04-11 21:59:17 +0530

    remove python completely, fix node backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { spawn } from "child_process";
 import jwt from "jsonwebtoken";
 import fetch from "node-fetch";
 
